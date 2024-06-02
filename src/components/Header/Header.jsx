7a48fc55680abd01f92ea2d8b948dd9112fe8afe
johnndi <159266362+johnndi@users.noreply.github.com>
2024-06-02 22:09:41 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import "./Header.css";
-import{link} from "react-router-dom";
+import { link } from "react-router-dom";
 const Header = () => {
   return (
     <section>
